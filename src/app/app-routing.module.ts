7ec50cb81b6394223346eaaceba32c28f01849d5
lucascagostinho <lucascagostinho@gmail.com>
2022-09-23 00:04:09 -0300

    new front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "header", component: HeaderComponent },
+  { path: "home", component: HomeComponent },
+  { path: "footer", component: FooterComponent },
+  { path: "about", component: AboutComponent },
+  { path: "login", component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
